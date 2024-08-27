e0f737a401167fa682de3a47516167b487e14f1f
etienne <etienne.pautet@gmail.com>
2024-08-27 18:41:34 -0300

    Project first code

---
+++ stock_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class computer_hardware(models.Model):
+    
+    name = models.CharField(max_length=50)
+    id_number = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
