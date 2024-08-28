900d2d059a53170e5fd807b7857c8b540b66f134
etienne <etienne.pautet@gmail.com>
2024-08-28 14:22:35 -0300

    Price and quantity added to models

--- stock_app/forms.py
+++ stock_app/forms.py
@@ -4,4 +4,4 @@ from .models import computer_hardware
 class computer_hardware_form(forms.ModelForm):
     class Meta:
         model = computer_hardware
-        fields = ('name', 'id_number')
+        fields = ('name', 'id_number', 'price', 'quantity')
--- stock_app/models.py
+++ stock_app/models.py
@@ -4,6 +4,10 @@ class computer_hardware(models.Model):
     
     name = models.CharField(max_length=50)
     id_number = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    quantity = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
+    
+
