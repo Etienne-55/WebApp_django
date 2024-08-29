dd7c2091e9f9a22b08816531adb7ed4b0c3a168d
etienne <etienne.pautet@gmail.com>
2024-08-29 18:56:13 -0300

    adicionar valor 0 a models

--- stock_app/models.py
+++ stock_app/models.py
@@ -4,8 +4,8 @@ class computer_hardware(models.Model):
     
     name = models.CharField(max_length=50)
     id_number = models.CharField(max_length=50)
-    price = models.CharField(max_length=50)
-    quantity = models.CharField(max_length=50)
+    price = models.CharField(max_length=50, default=0)
+    quantity = models.CharField(max_length=50, default=0)
 
     def __str__(self):
         return self.name
