71bb244e84a815ed79887c40ba53df439558ebf1
etienne <etienne.pautet@gmail.com>
2024-09-09 17:40:39 -0300

    remove unused files

--- stock_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import computer_hardware
-
-admin.site.register(computer_hardware)
-
--- stock_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import computer_hardware
-
-class computer_hardware_form(forms.ModelForm):
-    class Meta:
-        model = computer_hardware
-        fields = ('name', 'id_number', 'price', 'quantity')
