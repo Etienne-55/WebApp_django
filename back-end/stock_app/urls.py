43746af5559673a59d45a88af93fc69cef870a86
etienne <etienne.pautet@gmail.com>
2024-09-11 16:31:53 -0300

    Updated urls and views

--- back-end/stock_app/urls.py
+++ back-end/stock_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HelloWorld, Get_name, Get_password
+from .views import HelloWorld, Get_name, Get_password, Get_Email
 
 urlpatterns = [
     path('api/hello/', HelloWorld.as_view(), name='hello-world'),
     path('api/hello/', Get_name.as_view(), name='get-name'),
     path('api/hello/', Get_password.as_view(), name='get-password'),
+    path('api/hello_again/', Get_Email.as_view(), name='get-email')
 ]
--- back-end/stock_app/views.py
+++ back-end/stock_app/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 
 class HelloWorld(APIView):
     def get(self, request):
-        return Response({"message": "Hello, world!-test1"}, status=status.HTTP_200_OK)
+        return Response({"message": "Log-in or sign-in!! :)"}, status=status.HTTP_200_OK)
     
 class Get_name(APIView):
     def get(self, request):
@@ -13,4 +13,13 @@ class Get_name(APIView):
 class Get_password(APIView):
     def get(self, request):
         return Response({"message": "Enter your password: "}, status=status.HTTP_200_OK)
+    
+class Hello_again(APIView):
+    def get(self, request):
+        return Response({"message": "Please enter your information: "}, status=status.HTTP_200_OK)
+    
 
+class Get_Email(APIView):
+    def get(self, request):
+        return Response({"message": "Enter your email: "})
+    
